94559abf86fc1cbbc60ba915624496d25a2a2dfe
austinckohler <austinckohler@gmail.com>
2020-08-05 23:57:40 -0600

    added notes on types of modules that might be used in the app

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 console.log("hello world");
+
+//create several modules to keep each item seperated
+
+//budgetController
+//userInterfaceController
+//controller that can use both of the other modules
