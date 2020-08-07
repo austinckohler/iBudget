31efe02fe72419bba0a3a5b3d3e9b077a4e5fce7
austinckohler <austinckohler@gmail.com>
2020-08-06 22:12:28 -0600

    added functions and event listener

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ console.log("hello world");
 //budgetController
 //userInterfaceController
 //controller that can use both of the other modules
+
+let budgetController = (function () {})();
+
+var UIController = (function () {})();
+let controller = (function (budgetControl, UIControl) {
+  document.querySelector(".add__btn").addEventListener("click", function () {
+    // get input, add item to budget controller, add to ui, calc budget, and display
+    console.log("yo");
+  });
+})(budgetController, UIController);
