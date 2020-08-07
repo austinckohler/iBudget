f9ba20017db8424754f9b88ca1ecb576a3627cf7
austinckohler <austinckohler@gmail.com>
2020-08-07 15:02:36 -0600

    added income and exoense constructors, initialized application with init function, returns input values

--- index.js
+++ index.js
@@ -5,17 +5,85 @@ console.log("hello world");
 //budgetController
 //userInterfaceController
 //controller that can use both of the other modules
+var budgetController = (function () {
+  var Expense = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+  var Income = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+  var data = {
+    allItems: {
+      exp: [],
+      inc: [],
+    },
+    totals: {
+      exp: 0,
+      inc: 0,
+    },
+  };
 
-let budgetController = (function () {})();
-let controlAddItem = function () {};
+  return {
+    addItem: function (type, des, val) {
+      var newItem, ID;
+      ID = 0;
+      if (type === "exp") {
+        newItem = new Expense(ID, des, val);
+      } else if (type === "inc") {
+        newItem = new Income(ID, des, val);
+      } else {
+        console.log(error, "error");
+      }
+      data.allItems[type].push(newItem); // type is exp or inc
+      return newItem;
+    },
+  };
+})();
 
-var UIController = (function () {})();
-let controller = (function (budgetControl, UIControl) {
-  document.querySelector(".add__btn").addEventListener("click", controlAddItem);
+var UIController = (function () {
+  var DOMStrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    inputBtn: ".add__btn",
+  };
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMStrings.inputType).value, //either income or expense
+        description: document.querySelector(DOMStrings.inputDescription).value,
+        value: document.querySelector(DOMStrings.inputValue).value,
+      };
+    },
+    getDOMStrings: function () {
+      return DOMStrings;
+    },
+  };
+})();
+var controller = (function (budgetControl, UICtrl) {
+  var eventListeners = function () {
+    var DOM = UICtrl.getDOMStrings();
+    document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
 
-  document.addEventListener("keypress", function (event) {
-    if (event.keyCode === 13 || event.which === 13) {
-      controlAddItem();
-    }
-  });
+    document.addEventListener("keypress", function (event) {
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
+
+  var ctrlAddItem = function () {
+    var input = UICtrl.getInput;
+  };
+  return {
+    init: function () {
+      eventListeners();
+    },
+  };
 })(budgetController, UIController);
+
+controller.init();
