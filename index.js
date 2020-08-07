6a0e7b24bb81fbb32cfcc91e1a371dd06cefdee8
austinckohler <austinckohler@gmail.com>
2020-08-06 22:22:52 -0600

    added eventlistener for enter b utton

--- index.js
+++ index.js
@@ -7,11 +7,15 @@ console.log("hello world");
 //controller that can use both of the other modules
 
 let budgetController = (function () {})();
+let controlAddItem = function () {};
 
 var UIController = (function () {})();
 let controller = (function (budgetControl, UIControl) {
-  document.querySelector(".add__btn").addEventListener("click", function () {
-    // get input, add item to budget controller, add to ui, calc budget, and display
-    console.log("yo");
+  document.querySelector(".add__btn").addEventListener("click", controlAddItem);
+
+  document.addEventListener("keypress", function (event) {
+    if (event.keyCode === 13 || event.which === 13) {
+      controlAddItem();
+    }
   });
 })(budgetController, UIController);
